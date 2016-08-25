22b1f47f32477ad374b30ad946fde413f8a7983a
Anderson Santos <anderson.bcc.uag@gmail.com>
2016-08-25 17:04:12 -0300

    add model segmentation

--- projeto_pdi_web/core/models.py
+++ projeto_pdi_web/core/models.py
@@ -18,3 +18,19 @@ class Codification(models.Model):
         verbose_name = "Codificação"
         verbose_name_plural = "Codificações"
         ordering = ["created_at"]
+
+
+class Segmentation(models.Model):
+
+    image = models.ImageField(upload_to="upload/segmentation/input/", verbose_name="Imagem", blank=False)
+    image_cod = models.ImageField(upload_to="upload/segmentation/output/", verbose_name="Imagem", blank=True)
+    description = models.CharField("Descrição", max_length=50, blank=True)
+    created_at = models.DateTimeField("Criada em", auto_now_add=True)
+
+    def __str__(self):
+        return self.created_at
+
+    class Meta:
+        verbose_name = "Segmentação"
+        verbose_name_plural = "Segmentações"
+        ordering = ["created_at"]
