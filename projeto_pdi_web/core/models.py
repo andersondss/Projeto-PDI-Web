935f0e189ab9403dfab92e5f154f484a41af72bd
Anderson Santos <anderson.bcc.uag@gmail.com>
2016-09-07 22:45:13 -0300

    remove comments

--- projeto_pdi_web/core/models.py
+++ projeto_pdi_web/core/models.py
@@ -7,7 +7,6 @@ from django.conf import settings
 class Codification(models.Model):
 
     image = models.ImageField(upload_to="upload/codification/input/", verbose_name="Imagem", blank=False)
-    # image_cod = models.ImageField(upload_to="upload/codification/output/", verbose_name="Imagem", blank=True)
     created_at = models.DateTimeField("Criada em", auto_now_add=True)
 
     def __str__(self):
@@ -36,7 +35,6 @@ class Segmentation(models.Model):
 
     concept = models.IntegerField("Conceito", choices=concept_choice, default=0, blank=False)
     image = models.ImageField(upload_to="upload/segmentation/input/", verbose_name="Imagem", blank=False)
-    # description = models.CharField("Descrição", max_length=50, blank=True)
     created_at = models.DateTimeField("Criada em", auto_now_add=True)
 
     def __str__(self):
